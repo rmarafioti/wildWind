beb74d62f583eb4335b5c44e984db9a68284e368
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-05-28 21:35:21 -0500

    deposit form created and functional sans emailing user payment confirmation

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const { resolve } = require("path");
-require("dotenv").config({ path: "./.env" });
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "./.env" });
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-08-01",
 });
 
 app.use(express.static(process.env.STATIC_DIR));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -22,33 +24,25 @@ app.get("/config", (req, res) => {
 });
 
 app.post("/create-payment-intent", async (req, res) => {
-  console.log("Received amount from client:", req.body.amount);
-  const { amount } = req.body;
-
-  // Validate the amount to ensure it's a positive number and within reasonable bounds
-  if (!amount || amount <= 0 || amount > 10000) {
-    // Example: amount must not exceed 10,000 units
-    return res
-      .status(400)
-      .json({ error: { message: "Invalid amount specified" } });
+  console.log("Received amount:", req.body.amount);
+  const { amount } = req.body; // Retrieve the amount from the request body
+  if (!amount) {
+    return res.status(400).json({ error: { message: "Amount is required" } });
   }
-
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       currency: "USD",
-      amount: Math.round(amount * 100), // Convert amount to cents
-      payment_method_types: ["card"],
+      payment_method_types: m["card"],
+      amount: Math.round(amount * 100), // Convert amount to cents,
       automatic_payment_methods: { enabled: true },
     });
 
-    console.log("PaymentIntent created with ID:", paymentIntent.id); // Log PaymentIntent details
-
+    // Send publishable key and PaymentIntent details to client
     res.send({
       clientSecret: paymentIntent.client_secret,
     });
   } catch (e) {
-    console.error("Error creating PaymentIntent:", e);
-    res.status(400).send({
+    return res.status(400).send({
       error: {
         message: e.message,
       },
